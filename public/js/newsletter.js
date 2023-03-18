bc05b46ab0f9d058d2c511532426e6793b52392a
Osmar Valero <osmarvalero100@gmail.com>
2023-03-18 16:59:12 -0500

    Add validation email susctiption

---
+++ public/js/newsletter.js
@@ -0,0 +1,26 @@
+function validateEmail(email) {
+    const mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+    if(email.match(mailformat))
+        return true;
+    
+    return false;
+}
+
+const newsletterForm = document.getElementById('newsletter_form');
+
+if (newsletterForm) {
+    newsletterForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const formData = new FormData(newsletterForm);
+        const email = formData.get('email');
+
+        if (validateEmail(email)) {
+            alert('successful subscription.');
+            e.target.reset();
+        } else {
+            alert('Please enter a valid email.')
+        }
+        
+    });
+}
