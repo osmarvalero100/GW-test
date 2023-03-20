15b79149edf73f7509333fe607bbd58d6271a819
Osmar Valero <osmarvalero100@gmail.com>
2023-03-19 22:32:00 -0500

    create functionality to move sliders

--- public/js/slider.js
+++ public/js/slider.js
@@ -14,3 +14,64 @@ class Slider {
         slidersConatiner.appendChild(card);
     }
 }
+
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function getDataSlides(slider) {
+    let data = {}
+    const containerCard = slider.querySelector('.slider__slides');
+    const firstCard = containerCard.querySelector('.card');
+    const lastCard = containerCard.lastChild;
+    const spaceBetweenCards = 32;
+    const displacement = firstCard.clientWidth * 2 + spaceBetweenCards;
+    const currentTranslate = containerCard.style.translate.replace('px', '') || 0;
+
+    data.containerCard = containerCard;
+    data.displacement = parseInt(displacement);
+    data.currentTranslate = parseInt(currentTranslate);
+    data.firstCard = firstCard;
+    data.lastCard = lastCard;
+
+    return data;
+}
+
+const leftNavs = document.querySelectorAll('.slider .arrows__nav__left');
+
+if (leftNavs) {
+    leftNavs.forEach(left => {
+        left.addEventListener('click', (e) => {
+            const dataSlides = getDataSlides(e.target.closest('.slider'));
+            if (!isInViewport(dataSlides.firstCard)) {
+                const currentTranslate = dataSlides.currentTranslate;
+                const displacement = currentTranslate + dataSlides.displacement;
+                dataSlides.containerCard.style.translate = `${displacement}px`;
+            }
+            // const displacement = currentTranslate + dataSlides.displacement;
+            // dataSlides.containerCard.style.translate = `${displacement}px`;
+        });
+    })
+}
+
+const rightNavs = document.querySelectorAll('.slider .arrows__nav__right');
+
+if (rightNavs) {
+    rightNavs.forEach(right => {
+        right.addEventListener('click', (e) => {
+            const dataSlides = getDataSlides(e.target.closest('.slider'));
+            if (!isInViewport(dataSlides.lastCard)) {
+                const currentTranslate = dataSlides.currentTranslate;
+                const displacement = currentTranslate - dataSlides.displacement;
+                dataSlides.containerCard.style.translate = `${displacement}px`;
+            }
+        });
+    })
+}
+
