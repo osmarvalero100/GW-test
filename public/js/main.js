ee22480eb0a49c18a662e554881405334a81a1fd
Osmar Valero <osmarvalero100@gmail.com>
2023-03-20 19:14:44 -0500

    General mobile style settings

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,8 @@
+function isMobile() {
+    try{ document.createEvent("TouchEvent"); return true; }
+    catch(e){ return false; }
+}
+
 (async () => {
     const products = await getProducts();
 
@@ -7,9 +12,12 @@
         for (let i = 0; i < products.nodes.length; i++) {
             const product = products.nodes[i];
              
-            const coverSize = ((i % 2) == 0) ? 'card__cover' : 'card__cover card__cover--xl';
+            let coverSize = ((i % 2) == 0) ? 'card__cover' : 'card__cover card__cover--xl';
             const btnText = (product.totalInventory > 0) ? 'Add to cart' : 'See more';
             const ratings = product.tags.filter(tag => (!isNaN(tag) && tag != ''));
+
+            if (isMobile())
+                coverSize = coverSize.replace(' card__cover--xl', '');
 
             const card = new Card()
                 .setCover(product.featuredImage.url, coverSize)
